c0d935b006bf67c0dd6d5cc901a2801809ac5a09
Lee JaeEun <115062865+LeeJaeEun0@users.noreply.github.com>
2022-12-14 14:06:41 +0900

    update_tab

--- medical_institution/medical_institution.py
+++ medical_institution/medical_institution.py
@@ -21,35 +21,48 @@ if st.checkbox('원본 데이터 보기'):
     st.subheader('Raw data')
     st.write(df)
 
-# 일단 그래프 그려보기
-st.subheader('지역보건의료기관 시에 따른 분류')
-fig = plt.figure(figsize=(8, 4))
-sns.histplot(data=df, x='시도')
-st.pyplot(fig)
+tab1, tab2, tab3 = st.tabs(["시에 따른 분류", "시군구에 따른 분류", "보건기관별 안내"])
+
+with tab1:
+   # 일단 그래프 그려보기
+    st.subheader('지역보건의료기관 시에 따른 분류')
+    fig = plt.figure(figsize=(8, 4))
+    sns.histplot(data=df, x='시도')
+    st.pyplot(fig)
 
 
-# 데이터를 분석하기 # 리스트 출력
-st.subheader('시에 따른 분류')
-option = st.selectbox(
-    '시를 선택하세요', 
-    (df['시도'].drop_duplicates()))
+    # 데이터를 분석하기 # 리스트 출력
+    st.subheader('시에 따른 분류')
+    option = st.selectbox(
+        '시를 선택하세요', 
+        (df['시도'].drop_duplicates()))
 
-station_data = df.loc[(df['시도'] == option)]
-st.write(station_data)
-
-
-# 일단 그래프 그려보기
-st.subheader('지역보건의료기관 시에 따른 분류')
-fig = plt.figure(figsize=(8, 4))
-sns.histplot(data=df, x='시군구')
-st.pyplot(fig)
+    station_data = df.loc[(df['시도'] == option)]
+    st.write(station_data)
 
 
-# 데이터를 분석하기 # 리스트 출력
-st.subheader('시에 따른 분류')
-option = st.selectbox(
-    '시군구를 선택하세요', 
-    (df['시군구'].drop_duplicates()))
 
-station_data = df.loc[(df['시군구'] == option)]
-st.write(station_data)
+
+with tab2:
+    # 일단 그래프 그려보기 - 선택에 따라서 시안에 구만 보이게? 
+    st.subheader('지역보건의료기관 시에 따른 분류')
+    fig = plt.figure(figsize=(8, 4))
+    sns.histplot(data=df, x='시군구')
+
+    st.pyplot(fig)
+    # 데이터를 분석하기 # 리스트 출력 # 시 - 군구
+    st.subheader('시에 따른 분류')
+    option = st.selectbox(
+        '시군구를 선택하세요', 
+        (df['시군구'].drop_duplicates()))
+
+    station_data = df.loc[(df['시군구'] == option)]
+    st.write(station_data)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
+
+
+
